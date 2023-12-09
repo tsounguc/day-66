ede3acb657d6f03691e81bb369785e99e1a89c53
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-09 04:51:14 -0500

    turn rand_cafe into dictionary, then serialized it

--- main.py
+++ main.py
@@ -39,6 +39,21 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+def to_dict(self):
+    # Method 1.
+    dictionary = {}
+    # Loop through each column in the data record
+    for column in self.__table__.columns:
+        # Create a new dictionary entry;
+        # where the key is the name of the column
+        # and the value is the value of the column
+        dictionary[column.name] = getattr(self, column.name)
+    return dictionary
+
+    # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+    # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 with app.app_context():
     db.create_all()
 
@@ -54,22 +69,22 @@ def get_random_cafe():
         with app.app_context():
             cafes = db.session.execute(db.select(Cafe).order_by(Cafe.id)).scalars().all()
             random_cafe = random.choice(cafes)
-            return jsonify(
-                cafe={
-                    "can_take_calls": random_cafe.can_take_calls,
-                    "coffee_price": random_cafe.coffee_price,
-                    "has_sockets": random_cafe.has_sockets,
-                    "has_toilet": random_cafe.has_toilet,
-                    "has_wifi": random_cafe.has_wifi,
-                    "id": random_cafe.id,
-                    "img_url": random_cafe.img_url,
-                    "location": random_cafe.location,
-                    "map_url": random_cafe.map_url,
-                    "name": random_cafe.name,
-                    "seats": random_cafe.seats,
-                })
+            # return jsonify(
+            #     cafe={
+            #         "can_take_calls": random_cafe.can_take_calls,
+            #         "coffee_price": random_cafe.coffee_price,
+            #         "has_sockets": random_cafe.has_sockets,
+            #         "has_toilet": random_cafe.has_toilet,
+            #         "has_wifi": random_cafe.has_wifi,
+            #         "id": random_cafe.id,
+            #         "img_url": random_cafe.img_url,
+            #         "location": random_cafe.location,
+            #         "map_url": random_cafe.map_url,
+            #         "name": random_cafe.name,
+            #         "seats": random_cafe.seats,
+            #     })
 
-    pass
+            return jsonify(cafe=random_cafe.to_dict())
 
 
 # HTTP GET - Read Record
