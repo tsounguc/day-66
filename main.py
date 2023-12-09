bcc6336bf8c2822a291a07d062c18c2e05d3817e
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-09 16:34:36 -0500

    Post a new cafe

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class Cafe(db.Model):
     has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
+
     def to_dict(self):
         # Method 1.
         dictionary = {}
@@ -61,6 +62,7 @@ def home():
     return render_template("index.html")
 
 
+# HTTP GET - Read Record
 @app.route("/random", methods=['GET'])
 def get_random_cafe():
     if request.method == "GET":
@@ -104,9 +106,26 @@ def search():
             return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
 
 
-# HTTP GET - Read Record
-
 # HTTP POST - Create Record
+@app.route('/add', methods=['POST'])
+def post_new_cafe():
+    with app.app_context():
+        new_cafe = Cafe(
+            name=request.form.get("name"),
+            map_url=request.form.get("map_url"),
+            img_url=request.form.get("img_url"),
+            location=request.form.get("location"),
+            has_sockets=bool(request.form.get("sockets")),
+            has_toilet=bool(request.form.get("toilet")),
+            has_wifi=bool(request.form.get("wifi")),
+            can_take_calls=bool(request.form.get("calls")),
+            seats=request.form.get("seats"),
+            coffee_price=request.form.get("coffee_price"),
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
 
 # HTTP PUT/PATCH - Update Record
 
